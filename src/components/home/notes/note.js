84094a663fcf9496e8d7293da9058e6f050a4735
Bendik Skarpnes <Sharpness-B@users.noreply.github.com>
2024-04-25 09:00:25 +0200

    add comments

--- src/components/home/notes/note.js
+++ src/components/home/notes/note.js
@@ -24,22 +24,30 @@ const parser = new Parser();
 
 
 const feeds = [
-  { id:1, category: 'Norway', title: 'VG Forsiden', url: 'https://www.vg.no/rss/feed/forsiden/' },
+  // { id:1, category: 'Norway', title: 'VG Forsiden', url: 'https://www.vg.no/rss/feed/forsiden/' },
   { id:2, category: 'Norway', title: 'VG Innenriks', url: 'https://www.vg.no/rss/feed/?categories=1069' },
   { id:3, category: 'Norway', title: 'VG Utenriks', url: 'https://www.vg.no/rss/feed/?categories=1070' },
   { id:4, category: 'Norway', title: 'E24 – Alle nyheter', url: 'http://e24.no/rss2/' },
   { id:5, category: 'Norway', title: 'E24 – Børs og finans', url: 'https://e24.no/rss2/?seksjon=boers-og-finans' },
   // ... add more feeds here
-  { id:6, category: 'International', title: 'BBC Overview', url: 'https://www.bbc.com/news/10628494' },
+  // { id:6, category: 'International', title: 'BBC Overview', url: 'https://www.bbc.com/news/10628494' },
   { id:7, category: 'International', title: 'BBC World News', url: 'https://feeds.bbci.co.uk/news/world/rss.xml' },
   { id:8, category: 'International', title: 'BBC Top Stories', url: 'http://feeds.bbci.co.uk/news/rss.xml' },
-  { id:9, category: 'International', title: 'CNN Overview', url: 'https://edition.cnn.com/services/rss/' },
+  // { id:9, category: 'International', title: 'CNN Overview', url: 'https://edition.cnn.com/services/rss/' },
   { id:10, category: 'International', title: 'CNN Top Stories', url: 'http://rss.cnn.com/rss/edition.rss' },
 
   { id:11, category: 'Danish', title: 'Dansk', url: 'http://www.startside.nu/rss_feeds_kategori.php?action=category&cid=1' },
 
   // ... add more feeds here
 ];
+
+
+
+
+
+
+
+
 
 export default function Home() {
   const { user } = useUser();
@@ -70,7 +78,7 @@ export default function Home() {
     fetchSelectedFeeds();
   }, []); // Empty dependency array means this effect runs once on mount
 
-  //console.log(selectedFeeds)
+  
 
   const handleFeedClick = (feed) => {
     setSelectedFeeds((prevFeeds) => {
@@ -119,18 +127,22 @@ export default function Home() {
       // Filter out any feeds that failed to fetch
       const successfulFeedsData = allFeedsData.filter(Boolean);
       setFeedData(successfulFeedsData);
-
-      console.log(successfulFeedsData)
     };
     
     fetchFeeds();
   }, [selectedFeeds]);
 
+  // flatten all feeds into one item array
+  const items = feedData
+    .flatMap(feed => feed.data.items.map(item => ({ ...item, newspaper: feed.title })))
+    .sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
 
 
 
 
 
+
+    
   if (!user) return <Navigate to="/login" />;
 
   return (
@@ -164,11 +176,23 @@ export default function Home() {
         ))}
       </Box>
 
-      {/* Add feeds bar */}
+      {/* Add filter feeds bar */}
         {/* should be able to unclick and delete */}
 
-      {/* Read selected feeds */}
-      <Box sx={{ display: 'flex', overflowX: 'auto', p: 2 }}>
+      {/* Read selected feeds (one feed)*/}
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', p: 2 }}>
+        {items.map((item, index) => (
+          <Box key={index} sx={{ my: 1, p: 2, width: { xs: '100%', sm: '500px' }, backgroundColor: '#f6f6f6', borderRadius: '5px' }}>
+            <Typography variant="h4"        sx={{ fontFamily: 'Poppins', fontWeight: 600 }}>{item.title}</Typography>
+            <Typography variant="body2"     sx={{ fontFamily: 'Poppins', letterSpacing: '-0.8px', fontWeight: 300 }}>{new Date(item.pubDate).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' })} {item.newspaper}</Typography>
+            <Typography variant="subtitle1" sx={{ fontFamily: 'Poppins', letterSpacing: '-0.8px', fontWeight: 300 }}>{item.contentSnippet}</Typography>
+            <a href={item.link} target="_blank" rel="noreferrer" style={{ color: 'black', textDecoration: 'none' }}>Read more</a>
+          </Box>
+        ))}
+      </Box>
+
+      {/* Read selected feeds (one column per source)*/}
+      {/* <Box sx={{ display: 'flex', overflowX: 'auto', p: 2 }}>
         {feedData.map((feed, index) => (
           <Box 
             sx={{ 
@@ -190,7 +214,7 @@ export default function Home() {
             ))}
           </Box>
         ))}
-      </Box>
+      </Box> */}
     </>
   );
 }
