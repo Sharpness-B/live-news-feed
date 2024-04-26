bda1d831e2172f84658cfb2748f9bb30dbf446cd
Bendik Skarpnes <Sharpness-B@users.noreply.github.com>
2024-04-26 21:28:22 +0200

    settings under collapsable menue

--- src/components/home/notes/note.js
+++ src/components/home/notes/note.js
@@ -1,29 +1,23 @@
+import { useState, useEffect, createContext, useContext } from "react";
 import { Navigate } from "react-router-dom";
-// import { Grid } from "@mui/material";
-import NavBar from "../../common/navBar";
-import { useUser } from "../../../context/useUser";
+import { FormControl, InputLabel, Select, MenuItem, Snackbar, Alert } from '@mui/material';
+import { Grid, Typography, Chip, Box } from '@mui/material';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
+import NavBar from "../../common/navBar";
 import CustomFeedInput from './CustomFeedInput';
 import FilterBar from './FilterBar';
 import SelectFeeds from './SelectFeeds';
 import PayingUserModal from '../../common/PayingUserModal';
 
-import { createContext, useContext, useState, useEffect } from "react";
-import { FormControl, InputLabel, Select, MenuItem, Snackbar, Alert } from '@mui/material';
-import { Grid, Typography, Chip, Box } from '@mui/material';
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-
-
-
+import { useUser } from "../../../context/useUser";
 import { useInterval } from 'react-use';
-
-
-import hash from 'object-hash'; 
-
-
-
-
 import Parser from 'rss-parser';
+import hash from 'object-hash';
 
 
 // to read rss feeds
@@ -167,52 +161,58 @@ export default function Home() {
 
       
 
-      {/* Select feeds bar */}
-      <SelectFeeds user={user} selectedFeeds={selectedFeeds} setSelectedFeeds={setSelectedFeeds} setPayingUserModalVisible={setPayingUserModalVisible} />
-      
+      {/* Settings and filters */}
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>Innstillinger</Typography>
+        </AccordionSummary>
+        
+        <AccordionDetails>
+          {/* Select feeds bar */}
+          <SelectFeeds user={user} selectedFeeds={selectedFeeds} setSelectedFeeds={setSelectedFeeds} setPayingUserModalVisible={setPayingUserModalVisible} />
+          {/* Custom rss input */}
+          <CustomFeedInput user={user} setSelectedCustomFeeds={setSelectedCustomFeeds} setPayingUserModalVisible={setPayingUserModalVisible} />
+          {/* Filters */}
+          <FilterBar user={user} filters={filters} setFilters={setFilters} />
+        </AccordionDetails>
+      </Accordion>
 
-      {/* Custom rss input */}
-      <CustomFeedInput user={user} setSelectedCustomFeeds={setSelectedCustomFeeds} setPayingUserModalVisible={setPayingUserModalVisible} />
-
-      {/* Filters */}
-      <FilterBar user={user} filters={filters} setFilters={setFilters} />
 
 
+      {/* Read selected feeds (one feed)*/}
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', p: 2 }}>
+        {filtered_items.map((item, index) => {
+          let date;
+          let timeString;
+          let isNewArticle = false;
+          try {
+            date = new Date(item.pubDate);
+            const options = { hour: '2-digit', minute: '2-digit', hour12: false };
+            timeString = new Intl.DateTimeFormat('default', options).format(date);
+            // Check if the article is less than 5 minutes old
+            isNewArticle = (new Date() - date) < 5 * 60 * 1000;
+          } catch {}
 
-{/* Read selected feeds (one feed)*/}
-<Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', p: 2 }}>
-  {filtered_items.map((item, index) => {
-    let date;
-    let timeString;
-    let isNewArticle = false;
-    try {
-      date = new Date(item.pubDate);
-      const options = { hour: '2-digit', minute: '2-digit', hour12: false };
-      timeString = new Intl.DateTimeFormat('default', options).format(date);
-      // Check if the article is less than 5 minutes old
-      isNewArticle = (new Date() - date) < 5 * 60 * 1000;
-    } catch {}
+          const isOpen = openArticles[index];
 
-    const isOpen = openArticles[index];
+          const handleClick = () => {
+            const newOpenArticles = [...openArticles];
+            newOpenArticles[index] = !isOpen;
+            setOpenArticles(newOpenArticles);
+          };
 
-    const handleClick = () => {
-      const newOpenArticles = [...openArticles];
-      newOpenArticles[index] = !isOpen;
-      setOpenArticles(newOpenArticles);
-    };
-
-    return (
-      <Box key={index} sx={{ my: 0.5, p: 0.5, maxWidth: '1000px', width: '100%', backgroundColor: '#f6f6f6', borderRadius: '5px', cursor: 'pointer' }} onClick={handleClick}>
-        <Typography variant="subtitle1" sx={{ fontFamily: 'Poppins', fontWeight: 600, lineHeight: 1 }}>{item.title}</Typography>
-        <Typography variant="caption" sx={{ fontFamily: 'Poppins', letterSpacing: '-0.8px', fontWeight: 300, display: 'inline', lineHeight: 1 }}> 
-          {isNewArticle && <AccessAlarmIcon sx={{ fontSize: 16, verticalAlign: 'middle', color: 'red' }} />} {/* Display icon if the article is less than 5 minutes old */}
-          {timeString} {item.newspaper} <a href={item.link} target="_blank" rel="noreferrer" style={{ color: 'black', textDecoration: 'underline', paddingLeft: '5px' }}>Les mer</a>
-        </Typography>
-        {isOpen && <Typography variant="body2" sx={{ fontFamily: 'Poppins', letterSpacing: '-0.8px', fontWeight: 300, lineHeight: 1 }}>{item.contentSnippet}</Typography>}
-    </Box>
-    );
-  })}
-</Box>
+          return (
+            <Box key={index} sx={{ my: 0.5, p: 0.5, maxWidth: '1000px', width: '100%', backgroundColor: '#f6f6f6', borderRadius: '5px', cursor: 'pointer' }} onClick={handleClick}>
+              <Typography variant="subtitle1" sx={{ fontFamily: 'Poppins', fontWeight: 600, lineHeight: 1 }}>{item.title}</Typography>
+              <Typography variant="caption" sx={{ fontFamily: 'Poppins', letterSpacing: '-0.8px', fontWeight: 300, display: 'inline', lineHeight: 1 }}> 
+                {isNewArticle && <AccessAlarmIcon sx={{ fontSize: 16, verticalAlign: 'middle', color: 'red' }} />} {/* Display icon if the article is less than 5 minutes old */}
+                {timeString} {item.newspaper} <a href={item.link} target="_blank" rel="noreferrer" style={{ color: 'black', textDecoration: 'underline', paddingLeft: '5px' }}>Les mer</a>
+              </Typography>
+              {isOpen && <Typography variant="body2" sx={{ fontFamily: 'Poppins', letterSpacing: '-0.8px', fontWeight: 300, lineHeight: 1 }}>{item.contentSnippet}</Typography>}
+          </Box>
+          );
+        })}
+      </Box>
     </>
   );
 }
