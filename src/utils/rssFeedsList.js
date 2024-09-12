88d23d3e310486525c0180c1ac917edd9924372f
Bendik Skarpnes <Sharpness-B@users.noreply.github.com>
2024-09-12 17:47:46 +0200

    add more int and se news channels

--- src/utils/rssFeedsList.js
+++ src/utils/rssFeedsList.js
@@ -12,8 +12,29 @@ export const feeds = [
     { id:8, category: 'Internasjonal', title: 'BBC Top Stories', url: 'http://feeds.bbci.co.uk/news/rss.xml' },
     // { id:9, category: 'Internasjonal', title: 'CNN Overview', url: 'https://edition.cnn.com/services/rss/' },
     { id:10, category: 'Internasjonal', title: 'CNN Top Stories', url: 'http://rss.cnn.com/rss/edition.rss' },
+    { id:11, category: 'Internasjonal', title: 'NY Times', url: 'https://www.nytimes.com/svc/collections/v1/publish/https:/www.nytimes.com/section/world/rss.xml' },
+    { id:12, category: 'Internasjonal', title: 'Sky News', url: 'http://feeds.skynews.com/feeds/rss/world.xml' },
+    { id:13, category: 'Internasjonal', title: 'NBC News', url: 'http://feeds.nbcnews.com/nbcnews/public/news' },
+    { id:14, category: 'Internasjonal', title: 'CNBC', url: 'https://www.cnbc.com/id/100727362/device/rss/rss.html' },
+    { id:15, category: 'Internasjonal', title: 'The Guardian', url: 'https://www.theguardian.com/world/rss' },
+    { id:16, category: 'Internasjonal', title: 'Al Jazeera', url: 'https://www.aljazeera.com/xml/rss/all.xml' },
+    { id:17, category: 'Internasjonal', title: 'Fox News', url: 'https://moxie.foxnews.com/google-publisher/latest.xml' },
+    { id:18, category: 'Internasjonal', title: 'MarketWatch', url: 'https://feeds.content.dowjones.io/public/rss/mw_topstories'},
+
+
+
+    { id:30, category: 'Sverige', title: 'Dagens Nyheter', url: 'https://www.dn.se/rss/' },
+    { id:31, category: 'Sverige', title: 'Svenska Dagbladet', url: 'https://www.svd.se/feed/articles.rss' },
+    { id:32, category: 'Sverige', title: 'Nya Wermlands-Tidningen', url: 'https://www.nwt.se/feed/' },
+    { id:33, category: 'Sverige', title: 'Dagen', url: 'https://www.dagen.se/arc/outboundfeeds/rss/' },
+    { id:34, category: 'Sverige', title: 'Nyheter Idag', url: 'https://nyheteridag.se/feed/' },
+    { id:35, category: 'Sverige', title: 'SVT Nyheter', url: 'https://www.svt.se/rss.xml' },
   
     // { id:11, category: 'Dansk', title: 'Dansk', url: 'http://www.startside.nu/rss_feeds_kategori.php?action=category&cid=1' },
   
     // ... add more feeds here
 ];
+
+
+//Top 40 Sweden News RSS Feeds
+//https://rss.feedspot.com/sweden_news_rss_feeds/
